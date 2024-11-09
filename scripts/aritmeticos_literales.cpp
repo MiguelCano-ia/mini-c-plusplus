f400f13cb693cd9629578bf4930cb01b3ca511a8
VegaSyd <nicolas.vega@utp.edu.co>
2024-11-08 23:05:17 -0500

    vega

--- scripts/aritmeticos_literales.cpp
+++ scripts/aritmeticos_literales.cpp
@@ -3,8 +3,8 @@ int main() {
     float y = 3.14 * 2.0;
     int z = x % 2;
 
-    x++; //Comentario incremento
-    y--; //Comentario decremento
+    --x; //Comentario incremento
+    ++y; //Comentario decremento
     return 0;
 }
 
